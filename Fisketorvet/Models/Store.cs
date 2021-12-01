c211573b51ac57d1a859d838a0693b73304c067c
misa112 <michaela.macejovska@yahoo.com>
2021-12-01 12:53:09 +0100

    created class Store and added instance field

---
+++ Fisketorvet/Models/Store.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fisketorvet.Models
+{
+    public class Store
+    {
+        public int StoreId { get; set; }
+        public string StoreName { get; set; }
+        public enum StoreType { Shop, Restaurant }
+        public StoreType TheStoreType { get; set; }
+        public enum ShopType { Groceries, Fashion, HealthAndBeauty, BookAndElectronics, Services, Restaurant}
+        public ShopType TheShopType { get; set; }
+        public int TelContact { get; set; }
+        public TimeSpan OpeningTime { get; set; }
+        public string Offer { get; set; }
+        public string ImageName { get; set; }
+    }
+}
